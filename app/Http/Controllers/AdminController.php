263159024bc3751917c3260aa3273e12615bca0e
mdgolamrabbani <you@example.com>
2023-07-27 03:06:02 +0600

    Admin Dashbord + Admin Another Loging + Dash custo

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
-        return view('admin.admin_dashboard');
-    }//End Method   
+        return view('admin.index');
+    }//End Method  
+    
+    
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    } //end Mehthod
+
+    public function AdminLogin() {
+        return view('admin.admin_login');
+    }
+    
 }
