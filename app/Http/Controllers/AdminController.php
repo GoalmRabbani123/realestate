8371a01a32de759dde1ca1a6d36fa140e9210211
mdgolamrabbani <you@example.com>
2023-07-29 11:02:19 +0600

    admin profile and image  update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -27,12 +27,33 @@ class AdminController extends Controller
 
     public function AdminLogin() {
         return view('admin.admin_login');
-    }
+    }//End Method 
 
     public function AdminProfile(){
         $id = Auth::user()->id;
         $profileData = User::find($id);
         return view('admin.admin_profile_view', compact('profileData'));
-    }
+
+    }//End Method 
     
+
+    public function AdminProfileStore(Request $request){
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+        
+        if($request->file('photo')){
+            $file = $request->file('photo');
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['photo'] = $filename; 
+        } //end method
+        
+        $data->save();
+        return redirect()->back();
+    }
 }
